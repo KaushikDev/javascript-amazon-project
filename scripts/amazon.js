5b98c8d2eae4c11e2572da26b8dfd02dbee846f6
kaushik3pg <piyush.kaushik@3pillarglobal.com>
2026-02-11 14:06:57 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -70,6 +70,12 @@ document.querySelectorAll(".js-add-to-cart").forEach((btn) => {
         quantity: 1,
       });
     }
-    console.log(cart);
+
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    console.log(cartQuantity, cart);
   });
 });
